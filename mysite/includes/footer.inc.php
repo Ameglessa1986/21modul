ab570757883ebd58777c3ecf40445c7838422784
Alekseeva Anastasiya <faya-1023@mail.ru>
2021-11-18 16:32:57 +0300

    initial commit

---
+++ mysite/includes/footer.inc.php
@@ -0,0 +1,39 @@
+  <footer class="page-footer cyan">
+    <div id="contacts" class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">
+            Мы познакомились с основами PHP
+          </h5>
+          <p class="grey-text text-lighten-4">
+            Мы команда студентов, работающая над этим проектом!
+            Принимаем все ваши пожелания и замечания!
+          </p>
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Ссылки</h5>
+          <ul>
+            <li><a class="white-text" href="#!">Урок 1</a></li>
+            <li><a class="white-text" href="#!">Урок 2</a></li>
+            <li><a class="white-text" href="#!">Урок 3</a></li>
+            <li><a class="white-text" href="#!">Урок 4</a></li>
+          </ul>
+        </div>
+        <div class="col l3 s12">
+          <h5 class="white-text">Контакты</h5>
+          <ul>
+            <li><a class="white-text" href="#!">г.Краснодар</a></li>
+            <li><a class="white-text" href="#!">ул.Семфиропольская д.345</a></li>
+            <li><a class="white-text" href="#!">Офис 14</a></li>
+            <li><a class="white-text" href="#!">т. 8(961)2456988</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright blue">
+      <div class="container center-align">
+        Made by <a class="white-text" href="https://github.com/Ameglessa1986">Anastasiya Alekseeva</a>
+      </div>
+    </div>
+  </footer>
---
+++ mysite/includes/menu.inc.php
@@ -0,0 +1,29 @@
+<div class="navbar-fixed">
+  <nav class="blue">
+    <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    <!--  Navbar structure, visible on desktop -->
+    <div class="nav-wrapper">
+      <!--  Trigger sidenav  -->
+      <!-- Brand Logo -->
+      <a href="#" class="brand-logo">
+        <?php include 'logo.inc.php' ?>
+        PHP Skillfactory - M21</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a href="#task"> Об обучении </a></li>
+        <li><a href="https://github.com/Ameglessa1986"> Портфолио </a></li>
+
+        <li>
+          <a href="#info">Домашнее задание</a>
+        </li>
+
+        <li><a href="#"> Ссылки </a></li>
+        <li><a href="#contacts"> Контакты</a></li>
+      </ul>
+    </div>
+
+  </nav>
+</div>
+
+<?php
+  include 'sidenav.inc.php';
+?>
